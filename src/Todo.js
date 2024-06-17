2723089237f664ce9717df346925fa6d36e3c4bc
jjackchoi <jackchoi0325@gmail.com>
2024-06-17 18:24:02 +0900

    프) Todo 수정 완료

--- src/Todo.js
+++ src/Todo.js
@@ -4,6 +4,19 @@ import DeleteOutline from "@mui/icons-material/DeleteOutlined";
 
 const Todo = (props) => {
     const [item, setItem] = useState(props.item);
+    const [readOnly, setReadOnly] = useState(true);
+
+    const editItem = props.editItem;
+
+    const editEventHandler = (e) => {
+        item.title = e.target.value;
+        editItem();
+    }
+
+    const checkboxEventHandler = (e) => {
+        item.done = e.target.checked;
+        editItem();
+    }
 
     const deleteItem = props.deleteItem;
 
@@ -12,12 +25,29 @@ const Todo = (props) => {
         deleteItem(item);
     }
 
+    // turnOffReadOnly 함수 작성
+    const turnOffReadOnly = () => {
+        setReadOnly(false);
+    }
+
+    // turnOnReadOnly 함수 작성
+    const turnOnReadOnly = (e) => {
+        if (e.key === "Enter") {
+            setReadOnly(true);
+        }
+    }
+
     return (
         <ListItem>
-            <Checkbox checked={item.done} />
+            <Checkbox checked={item.done}  onChange={checkboxEventHandler}/>
             <ListItemText>
                 <InputBase 
-                    inputProps={{ "aria-label": "naked" }}
+                    inputProps={{
+                        "aria-label": "naked", 
+                        readOnly: readOnly }}
+                    onClick={ turnOffReadOnly }
+                    onKeyDown={ turnOnReadOnly }
+                    onChange={ editEventHandler }
                     type="text"
                     id={item.id}
                     name={item.id}
